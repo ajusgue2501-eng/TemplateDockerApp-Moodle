488dbdede7b0113e7a1e288c8c6921b9f4f6b7ad
Alexis Justo Guerra <ajusgue2501@g.educaand.es>
2025-12-17 16:05:28 +0100

    Comentarios añadidos a sentencia sql para crear esquema inicial

--- docker/bd/sql/init.dev.sql
+++ docker/bd/sql/init.dev.sql
@@ -1,3 +1,6 @@
+-- Creamos el esquema si no existe
+-- Se llama template_docker_app_dev y se define con charset utf8mb4 y colacion utf8mb4_general_ci
 CREATE SCHEMA IF NOT EXISTS `template_docker_app_dev` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
+-- Indicamos que vamos a usar el esquema recien creado
 USE `template_docker_app_dev`;
